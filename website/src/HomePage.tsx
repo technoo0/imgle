2b101eef53287ba49b42c077688ce72e8c0f017e
Marwan Fouda <65347532+technoo0@users.noreply.github.com>
2022-02-01 23:36:41 +0200

    reset on drag

--- website/src/HomePage.tsx
+++ website/src/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import DragAndDrop from "./components/DragAndDrop";
 import Header from "./components/Header";
@@ -10,6 +10,17 @@ export default function HomePage() {
   const handleChange = (newfile: File) => {
     setFile(newfile);
   };
+  useEffect(() => {
+    const ResetFile = () => {
+      if (file) {
+        setFile(null);
+      }
+    };
+    window.addEventListener("dragover", ResetFile);
+    return () => {
+      window.removeEventListener("dragover", ResetFile);
+    };
+  }, [file, setFile]);
   const GoBack = () => {
     setFile(null);
   };
